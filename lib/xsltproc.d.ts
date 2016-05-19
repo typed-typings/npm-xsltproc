c9124dcca37b5c9bc49dfefebe48a92d32411a57
Homa Wong <homawong@gmail.com>
2016-05-18 23:53:29 -0700

    Make option fields optional

--- lib/xsltproc.d.ts
+++ lib/xsltproc.d.ts
@@ -2,11 +2,11 @@ import process = require('child_process');
 
 declare namespace xsltproc {
   export interface Option {
-    xinclude: any;
-    novalid: any;
-    profile: any;
-    output: string;
-    encoding: string;
+    xinclude?: any;
+    novalid?: any;
+    profile?: any;
+    output?: string;
+    encoding?: string;
   }
 
   export function transform(stylesheet: string, file: string, options?: Option): process.ChildProcess
